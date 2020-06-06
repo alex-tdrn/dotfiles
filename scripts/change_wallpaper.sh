d352b335d021ebf151edae629e1522b94f0b4947
Alex Todoran <alex-tdrn@protonmail.com>
2020-06-06 14:38:39 +0200

    modified change background script to take extra parameter for wal

--- scripts/change_wallpaper.sh
+++ scripts/change_wallpaper.sh
@@ -1,3 +1,3 @@
 bg=$(find /media/common/wallpapers -iname '*.jpg' -print0 | shuf -z -n1)
-wal -e -q -n -l -i $bg --saturate 0.25
+wal $1 -e -q -n -i $bg --saturate 0.25
 swaymsg output "*" background $bg fill
